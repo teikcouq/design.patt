0ac7899e83e074ccc2b2f8e4dffbbb3d924a6db0
Kiet Ly <kiet_dpm215476@student.agu.edu.vn>
2024-09-17 19:21:39 +0700

    Create .dtbcache.v2

---
+++ Creational Design Pattern/Singleton/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton.RealWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+    public class LoadBalancer
+    {
+        static LoadBalancer instance;
+        List<string> servers = new List<string>();
+        Random random = new Random();
+
+        // Lock synchronization object
+
+        private static object locker = new object();
+
+        // Constructor (protected)
+
+        protected LoadBalancer()
+        {
+            // List of available servers
+            servers.Add("ServerI");
+            servers.Add("ServerII");
+            servers.Add("ServerIII");
+            servers.Add("ServerIV");
+            servers.Add("ServerV");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return instance;
+        }
+
+        public string Server
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r].ToString();
+            }
+        }
+    }
+}
